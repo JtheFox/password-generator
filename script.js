367bfe7483d5d1331bc373c29d2a833a9d1bf89e
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-05-19 12:21:44 -0400

    Initial code for criteria based  regex password generation

--- script.js
+++ script.js
@@ -8,14 +8,22 @@ const rgxMap = new Map()
 function generatePassword() {
     // input validation to check if user has at least one checkbox selected
     // code snippet adapted from https://stackoverflow.com/questions/11787665/making-sure-at-least-one-checkbox-is-checked
-    let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
     if (!Array.prototype.slice.call(checkboxes).some(item => item.checked)) {
-        document.getElementsByClassName('error')[0].textContent = "You must select at least one item.";
+        document.getElementsByClassName('error')[0].textContent = 'You must select at least one item.';
         return;
     } else {
         // reset error if user resubmits with at least one item selected
-        document.getElementsByClassName('error')[0].textContent = ""; 
+        document.getElementsByClassName('error')[0].textContent = ''; 
     }
     
-    
+    let passRgx = '';
+    // use map to add values from checkboxes to regex string
+    for (let item of checkboxes) {
+        if (item.checked) {
+            passRgx += rgxMap.get(item.value);
+        }
+    }
+
+    document.getElementById('generated').textContent = new RandExp(passRgx).gen();;
 }
