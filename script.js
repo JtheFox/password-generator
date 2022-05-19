37325f174889d701db1055b6dc690efe6ec88375
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-05-19 11:58:14 -0400

    Added regex map for criteria

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+// create map for checkbox values to regex
+const rgxMap = new Map()
+    .set('lowercase', '[a-z]')
+    .set('uppercase', '[A-Z]')
+    .set('numbers', '[0-9]')
+    .set('special-characters', '[!"#$%&\'()*+,-./:;<=>?@[\\]^ _`{|}~]'); 
+
 function generatePassword() {
-    // Code snippet adapted from https://stackoverflow.com/questions/11787665/making-sure-at-least-one-checkbox-is-checked
+    // input validation to check if user has at least one checkbox selected
+    // code snippet adapted from https://stackoverflow.com/questions/11787665/making-sure-at-least-one-checkbox-is-checked
     let checkboxes = document.querySelectorAll('input[type="checkbox"]');
     if (!Array.prototype.slice.call(checkboxes).some(item => item.checked)) {
         document.getElementsByClassName('error')[0].textContent = "You must select at least one item.";
+        return;
+    } else {
+        // reset error if user resubmits with at least one item selected
+        document.getElementsByClassName('error')[0].textContent = ""; 
     }
+    
+    
 }
